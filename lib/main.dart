1590da8da237f6811b170e78c273a3caa5a38378
khaledabdelrazeq <khaled.abdelrazeq971@gmail.com>
2026-02-23 00:08:11 +0200

    Add feature configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:reta/features/auth/presentation/pages/home_page.dart';
-import 'package:reta/features/auth/presentation/pages/settings_page.dart';
+
 import 'core/theme/app_theme.dart';
 
 void main() {
@@ -12,16 +13,22 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme.lightTheme,
-      // For RTL/LTR support:
-      // localizationsDelegates: const [
-      //   GlobalMaterialLocalizations.delegate,
-      //   GlobalWidgetsLocalizations.delegate,
-      //   GlobalCupertinoLocalizations.delegate,
-      // ],
-      supportedLocales: const [Locale('en'), Locale('ar')],
-      home: SettingsPage(),
+    return ScreenUtilInit(
+      designSize: const Size(440, 956),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        theme: AppTheme.lightTheme,
+        // For RTL/LTR support:
+        // localizationsDelegates: const [
+        //   GlobalMaterialLocalizations.delegate,
+        //   GlobalWidgetsLocalizations.delegate,
+        //   GlobalCupertinoLocalizations.delegate,
+        // ],
+        debugShowCheckedModeBanner: false,
+        supportedLocales: const [Locale('en'), Locale('ar')],
+        home: HomePage(),
+      ),
     );
   }
 }
