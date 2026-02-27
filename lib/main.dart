9ec525c20d22e46ceaaa373c2cb2b62bf9745dd4
khaledabdelrazeq <khaled.abdelrazeq971@gmail.com>
2026-02-28 01:34:13 +0200

    Revert main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:reta/features/auth/presentation/pages/guest_page.dart';
 
-import 'core/helpers/app_enum.dart';
 import 'core/theme/app_theme.dart';
-import 'features/declarations/presentations/cubit/declaration_lookups_cubit.dart';
-import 'features/declarations/presentations/pages/provider_data_page.dart';
+import 'features/splash/presentation/pages/splash.dart';
 
 void main() {
   runApp(const MainApp());
@@ -32,14 +29,14 @@ class MainApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         supportedLocales: const [Locale('ar'), Locale('en')],
         builder: (context, Widget? child) => child!,
-        // home: SplashPage(),
-        home: BlocProvider(
-          create: (_) => DeclarationLookupsCubit()..fetchLookups(),
-          child: ProviderDataPage(
-            applicantType: ApplicantType.owner,
-            declarationId: 70,
-          ),
-        ),
+        home: SplashPage(),
+        // home: BlocProvider(
+        //   create: (_) => DeclarationLookupsCubit()..fetchLookups(),
+        //   child: ProviderDataPage(
+        //     applicantType: ApplicantType.owner,
+        //     declarationId: 70,
+        //   ),
+        // ),
         // home: MainPage(),
         routes: {'/home': (context) => GuestPage()},
       ),
