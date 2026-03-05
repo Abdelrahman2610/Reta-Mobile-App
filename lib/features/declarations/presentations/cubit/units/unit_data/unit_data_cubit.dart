96dae8ad0f923d0983cb026d2b8c4a1ddb32aab4
khaledabdelrazeq <khaled.abdelrazeq971@gmail.com>
2026-03-05 19:33:04 +0200

    Update building numbers

--- lib/features/declarations/presentations/cubit/units/unit_data/unit_data_cubit.dart
+++ lib/features/declarations/presentations/cubit/units/unit_data/unit_data_cubit.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -1398,7 +1396,6 @@ class UnitDataCubit extends Cubit<UnitDataState> {
         },
       };
 
-      log('unitPayload: ${_buildUnitPayload(unitType, lookups)}');
       final result = isEdit
           ? await DeclarationService.instance.updateDeclaration(
               payload,
@@ -1571,7 +1568,6 @@ class UnitDataCubit extends Cubit<UnitDataState> {
         )
         .id;
 
-    log('state.isTaxpayerOwner: ${state.isTaxpayerOwner}');
     return {
       ..._buildBaseUnitPayload(),
       'installation_type_id': installationTypeId,
