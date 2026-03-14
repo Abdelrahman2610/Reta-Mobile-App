6a2e6317969d1991ffa91117158ae02d8f809d40
Marco M.Nabih <marco.nabih@Marcos-MacBook-Pro.local>
2026-03-14 21:31:18 +0200

    fix bugs

--- lib/features/declarations/presentations/cubit/units/unit_data/unit_data_cubit.dart
+++ lib/features/declarations/presentations/cubit/units/unit_data/unit_data_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -1610,7 +1612,7 @@ class UnitDataCubit extends Cubit<UnitDataState> {
           ..._buildUnitPayload(unitType, lookups),
         },
       };
-
+      log("payload: ${payload.toString()}");
       final result = isEdit
           ? await DeclarationService.instance.updateDeclaration(
               payload,
